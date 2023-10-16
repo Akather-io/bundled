f9aba8cba650e65d67548d6d59fa655487ba5f35
tronginc <tronginc@gmail.com>
2023-10-16 13:53:45 +0700

    list basic-strategy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <div className="text-center">
-        <h2 className="text-xl font-bold leading-7">Suitable Strategies</h2>
-        <p className="mt-1 text-xl leading-6 text-indigo-500">Choose your strategy</p>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
